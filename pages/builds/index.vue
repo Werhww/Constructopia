d88e31f21e52e8df1c2ba3d3756d48ad3e570439
Werhww <kleppeleo@gmail.com>
2023-08-19 22:38:14 +0200

    update

--- pages/builds/index.vue
+++ pages/builds/index.vue
@@ -12,20 +12,20 @@
 
 
 <script setup lang="ts">
+const route = useRoute()
+const testList = ref<any>([])
 definePageMeta({
   title: 'Home',
 })
-
-const route = useRoute()
 
 useHead({
   meta: [{ property: 'og:title', content: `Constructopia - ${route.meta.title}` }]
 })
 
-const testList = ref<any>([])
+
 
 onMounted(async () => {
-  testList.value = await getBuildListByCategory('1234test')
+  testList.value = await getBuildListByCategory(TestUserId)
   console.log(testList.value)
 })
 </script>
