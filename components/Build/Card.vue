c3cf853f197f5872b8fde8664fd8adb34cff4578
werhw <kleppeleo@gmail.com>
2024-03-18 10:27:28 +0100

    Update border radius and card layout, fix syntax error, and remove unused file

--- components/Build/Card.vue
+++ components/Build/Card.vue
@@ -3,8 +3,8 @@ const props = defineProps<{
   id: number;
 }>();
 
-const data = await serverFunction("findBuild", props.id)
-if (isServerError(data)) throw createError("Build not found")
+const data = await serverFunction("findBuild", props.id);
+if (isServerError(data)) throw createError("Build not found");
 </script>
 
 <template>
@@ -20,71 +20,90 @@ if (isServerError(data)) throw createError("Build not found")
       anchor="center end"
       self="top left"
       class="q-pa-none"
-
-      maxWidth="350px"
+      maxWidth="400px"
     >
       <QCard flat dark bordered>
         <QCardSection class="q-pb-none">
           <div class="text-h6 ellipsis-2-lines">{{ data.title }}</div>
-          <div class="text-subtitle2 text-grey-7 text-weight-bold">
-            by {{ data.username }}
+          <div class="q-pl-sm text-subtitle2 text-grey-7 text-weight-bold">
+            by @{{ data.username }}
           </div>
           <QSeparator dark spaced="sm" />
         </QCardSection>
-        <QCardSection class="q-pt-none q-pb-none q-pl-md row items-center q-gutter-sm">
-          <QChip dark dense ripple icon="sym_r_download" class="q-pa-none text-grey-7 text-subtitle1 text-weight-medium" >
-            {{ data.downloads }}
-          </QChip>
-          <QChip dark dense ripple icon="sym_r_visibility" class="q-pa-none text-grey-7 text-subtitle1 text-weight-medium" >
-            {{ data.views }}
-          </QChip>
-          <QChip dark dense ripple icon="sym_s_deployed_code" class="q-pa-none text-grey-7 text-subtitle1 text-weight-medium" >
-            {{ data.blockCount }}
-          </QChip>
-          <QChip dark dense ripple icon="sym_r_height" class="q-pa-none text-grey-7 text-subtitle1 text-weight-medium" >
-            {{ data.size }}
-          </QChip>
-          <span class="text-grey-7 text-subtitle1 text-weight-medium">
-            /{{ data.difficulty }}
-          </span>
+
+        <QCardSection horizontal class="row q-pb-sm q-pt-sm">
+          <QCardSection
+            class="q-pa-none column col items-center text-body2 text-grey-6"
+          >
+            <div>Blocks</div>
+
+            <div class="text-weight-bold">{{ data.blockCount }}</div>
+          </QCardSection>
+          <QSeparator vertical dark />
+          <QCardSection
+            class="q-pa-none column col items-center text-body2 text-grey-6"
+          >
+            <div>Difficulty</div>
+
+            <div class="text-weight-bold">{{ data.difficulty }}</div>
+          </QCardSection>
+          <QSeparator vertical dark />
+          <QCardSection
+            class="q-pa-none column col items-center text-body2 text-grey-6"
+          >
+            <div>Size</div>
+
+            <div class="text-weight-bold">{{ data.size }}</div>
+          </QCardSection>
         </QCardSection>
 
         <QCardSection class="q-pt-none q-pb-xs">
-          <div class="text-body2 text-grey-6">
-            Created at
-            <span class="text-weight-bold">{{
-              useDateFormat(data.createdAt, "MMM D, YYYY").value
-            }}</span>
-          </div>
-          <div class="text-body2 text-grey-6">
-            Last changed at
-            <span class="text-weight-bold">{{
-              useDateFormat(data.updateAt, "MMM D, YYYY").value
-            }}</span>
-          </div>
-          <div class="row q-gutter-xs text-body2 text-grey-6">
-            <div>Categorys:</div>
-            <div class="row" v-for="(category, i) in data.categorys">
-              <div class="text-weight-bold">{{ category.title }}</div>
-              <div v-if="i != (data.categorys.length - 1)">,</div>
+          <div class="row q-gutter-xl text-body2 text-grey-6">
+            <div>Created at</div>
+            <QSpace />
+            <div class="text-weight-bold">
+              {{ useDateFormat(data.createdAt, "D MMM YYYY").value }}
             </div>
           </div>
+          <div class="row q-gutter-xl text-body2 text-grey-6">
+            <div>Last change</div>
+            <QSpace />
+            <div class="text-weight-bold">
+              {{ useDateFormat(data.updateAt, "D MMM YYYY").value }}
+            </div>
+          </div>
+          <div class="row q-gutter-xl text-body2 text-grey-6">
+            <div>Download</div>
+            <QSpace />
+            <div class="text-weight-bold">{{ data.downloads }}</div>
+          </div>
+          <div class="row q-gutter-xl text-body2 text-grey-6">
+            <div>Views</div>
+            <QSpace />
+            <div class="text-weight-bold">{{ data.views }}</div>
+          </div>
         </QCardSection>
-        <QCardSection class="q-pt-none text-grey-8 text-body2">
-          <div class="descriptionEllipsis">
+
+        <QCardSection class="q-pt-none q-pb-xs text-grey-8 text-body2">
+          <div class="ellipsis-3-lines">
             {{ data.description }}
+          </div>
+        </QCardSection>
+
+        <QCardSection class="q-pt-sm q-pb-md">
+          <div class="row items-center q-gutter-xs text-body2 text-grey-6">
+            <div class="text-justify text-center" style="height: 28px">
+              Categorys:
+            </div>
+            <QChip
+              square
+              class="q-pa-sm bg-grey-9 text-grey text-weight-medium"
+              v-for="category in data.categorys"
+              :label="category.title"
+            />
           </div>
         </QCardSection>
       </QCard>
     </QTooltip>
   </QCard>
 </template>
-
-<style scoped lang="scss">
-.descriptionEllipsis {
-  display: -webkit-box;
-  -webkit-line-clamp: 5;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-}
-</style>
